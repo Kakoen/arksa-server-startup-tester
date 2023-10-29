ebd471d70021c6c47364a354472a619c78dbbb40
kakoen <kakoen@gmail.com>
2023-10-29 12:42:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+h1. Ark: Survival Ascended - Server tester
+
+**Warning! Only to help the tech savvy. I'm using this for my own server, and decided to publish it to help others. But I don't
+have the time to make it user friendly / deliver support on it. If you're not a developer, you probably won't be able
+to use this.**
+
+I will not be held responsible for any damage caused by this script. Use at your own risk.
+
+A common problem for Ark: SA is the server ending up in a crash loop.
+
+If you then try to load up the save files (including *.arktribe and *.arkprofile files) in a Single Player game, you 
+will see a stacktrace. If that stacktrace includes loading tribe files, then there's a big chance one of your tribe files 
+is corrupted.
+
+This script tries to find the tribe files that are corrupt by repeatedly loading the server with a specific set of 
+tribe files, and narrowing down to the corrupt files, using binary search. This means you'll need around LOG(n) server restarts
+to find the corrupt tribe files, where n is the number of tribes.
+
+# Usage
+
+1. Backup any save files, as this script will delete the destination folder with every try.
+2. Put your save files in the 'save_files' directory.
+3. Modify Main.java with the correct paths to your dedicated server installation.
+4. Clear bad_tribes.txt and good_tribes.txt if needed (this is used to persist and continue a single test run)
+5. Run the script. It will start the server with all tribes, and then narrow down to the corrupt ones. It will print the corrupt tribes in the end.
---
+++ src/main/java/net/kakoen/Main.java
@@ -0,0 +1,171 @@
+package net.kakoen;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.time.Instant;
+import java.util.*;
+import java.util.function.Predicate;
+
+@Slf4j
+public class Main {
+    static final File SAVE_DEST = new File("D:\\SteamLibrary\\steamapps\\common\\ARK Survival Ascended Dedicated Server\\ShooterGame\\Saved\\SavedArks\\TheIsland_WP");
+    static final File SAVE_SRC = new File("save_files");
+    static final File SERVER_BINARY = new File("D:\\SteamLibrary\\steamapps\\common\\ARK Survival Ascended Dedicated Server\\ShooterGame\\Binaries\\Win64\\ArkAscendedServer.exe");
+    static final String GOOD_TRIBES_FILE = "good_tribes.txt";
+    static final String BAD_TRIBES_FILE = "bad_tribes.txt";
+    static final int ASSUME_SUCCESSFUL_STARTUP_SECONDS = 120;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        if(!SAVE_DEST.getName().equals("TheIsland_WP")) {
+            log.error("Make sure the SAVE_DEST files points to the correct TheIsland_WP directory (ShooterGame\\Saved\\SavedArks\\TheIsland_WP in your server installation)!  This folder WILL be wiped!");
+            return;
+        }
+
+        Set<String> goodTribes = readFromFile(GOOD_TRIBES_FILE);
+        Set<String> badTribes = readFromFile(BAD_TRIBES_FILE);
+
+        List<String> tribeFiles = Arrays.stream(Objects.requireNonNull(SAVE_SRC.listFiles()))
+                .map(File::getName)
+                .filter(name -> name.endsWith("arktribe"))
+                .filter(name -> !goodTribes.contains(name))
+                .filter(name -> !badTribes.contains(name))
+                .toList();
+
+        log.info("Assuming good tribes from earlier run: {}", goodTribes);
+        log.info("Assuming bad tribes from earlier run: {}", badTribes);
+        log.info("Other tribe files: {}", tribeFiles);
+
+        Stack<List<String>> tribeFilesStack = new Stack<>();
+
+        while(!tribeFiles.isEmpty()) {
+            //Delete directory SAVE_DEST recursively
+            if (!deleteDirectory(SAVE_DEST) && SAVE_DEST.exists()) {
+                log.error("Failed to delete directory {}, trying again in 5 seconds...", SAVE_DEST);
+                Thread.sleep(5000);
+                continue;
+            }
+
+            if (!SAVE_DEST.mkdirs()) {
+                log.error("Failed to create directory {}", SAVE_DEST);
+                return;
+            }
+
+            final List<String> tribeFilesCopy = new ArrayList<>(tribeFiles);
+            tribeFilesCopy.addAll(goodTribes);
+            copyDirectory(SAVE_SRC, SAVE_DEST, (file) -> !file.getName().endsWith("arktribe") || tribeFilesCopy.contains(file.getName()));
+
+            log.info("=====================================================");
+            log.info("Running server with tribes {}", tribeFiles);
+            log.info("Good tribes {}", goodTribes);
+            log.info("Bad tribes {}", badTribes);
+            log.info("Stack {}", tribeFilesStack.size());
+            Process process = new ProcessBuilder(SERVER_BINARY.getAbsolutePath(), "TheIsland_WP?listen", "-UseBattlEye").directory(SERVER_BINARY).start();
+            Instant startDate = Instant.now();
+            boolean saveWorks = false;
+            while(process.isAlive()) {
+                if (Instant.now().isAfter(startDate.plusSeconds(ASSUME_SUCCESSFUL_STARTUP_SECONDS))) {
+                    saveWorks = true;
+                    log.info("Process did not exit after {} seconds, succesfull test! Killing server...", ASSUME_SUCCESSFUL_STARTUP_SECONDS);
+                    process.destroyForcibly();
+                }
+                Thread.sleep(1000);
+            }
+            log.info("Process exited with code {} after {} seconds", process.exitValue(), Instant.now().getEpochSecond() - startDate.getEpochSecond());
+
+            if(!saveWorks) {
+                if(tribeFiles.size() == 1) {
+                    log.info("Bad tribe found and isolated: {}", tribeFiles);
+                    badTribes.addAll(tribeFiles);
+                    writeToFile(BAD_TRIBES_FILE, badTribes);
+
+                    if(tribeFilesStack.isEmpty()) {
+                        log.info("No more tribes to test!");
+                        log.info("Bad tribes: {}", badTribes);
+                        return;
+                    }
+                } else {
+                    //remove half of the tribes and put them on the stack
+                    int half = tribeFiles.size() / 2;
+                    List<String> removedTribes = tribeFiles.subList(0, half);
+                    tribeFiles = tribeFiles.subList(half, tribeFiles.size());
+                    tribeFilesStack.push(removedTribes);
+                    log.info("Put tribes on the stack for later testing {}", removedTribes);
+                }
+            } else {
+                log.info("Save works!");
+                log.info("Added good tribes: {}", tribeFiles);
+                goodTribes.addAll(tribeFiles);
+                writeToFile(GOOD_TRIBES_FILE, goodTribes);
+                if(!tribeFilesStack.isEmpty()) {
+                    tribeFiles = new ArrayList<>(tribeFilesStack.pop());
+                } else {
+                    log.info("Save works, no more tribes to test!");
+                    log.info("Bad tribes: {}", badTribes);
+                    return;
+                }
+            }
+        }
+
+
+
+    }
+
+    private static void writeToFile(String fileName, Set<String> goodTribes) {
+        try {
+            Files.writeString(new File(fileName).toPath(), String.join("\n", goodTribes));
+        } catch (IOException e) {
+            log.error("Failed to write to file {}", fileName, e);
+        }
+    }
+
+    private static Set<String> readFromFile(String fileName) {
+        if(!new File(fileName).exists()) {
+            return new HashSet<>();
+        }
+        try {
+            return new HashSet<>(Files.readAllLines(new File(fileName).toPath()));
+        } catch (IOException e) {
+            log.error("Failed to read from file {}", fileName, e);
+            return new HashSet<>();
+        }
+    }
+
+    private static boolean copyDirectory(File saveSrc, File saveDest, Predicate<File> fileFilter) throws IOException {
+        if (saveSrc.isDirectory()) {
+            File[] files = saveSrc.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (!copyDirectory(file, saveDest, fileFilter)) {
+                        return false;
+                    }
+                }
+            }
+        } else {
+            if (fileFilter.test(saveSrc)) {
+                File destFile = new File(saveDest, saveSrc.getName());
+                destFile.getParentFile().mkdirs();
+                Files.copy(saveSrc.toPath(), destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
+        return true;
+    }
+
+    private static boolean deleteDirectory(File saveDest) {
+        if (saveDest.isDirectory()) {
+            File[] files = saveDest.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    deleteDirectory(file);
+                }
+            }
+        }
+        return saveDest.delete();
+    }
+
+
+}
